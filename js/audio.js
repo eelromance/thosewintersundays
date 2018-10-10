58036492520111acdca32d2365745e091b181369
Cameron Lee <cameronlee@Camerons-MacBook-Pro.local>
2018-10-09 23:40:59 -0500

    intial commit

---
+++ js/audio.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/intro.wav";
+    obj.volume=1;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#intro").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/one.wav";
+    obj.volume=1;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#one").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/two.wav";
+    obj.volume=1;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#two").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/three.wav";
+    obj.volume=1;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#three").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/four.wav";
+    obj.volume=1;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#four").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/five.wav";
+    obj.volume=1;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#five").click(function() {
+        obj.play();
+    });
+});
+
+$(document).ready(function() {
+    var obj = document.createElement("audio");
+    obj.src="/aud/six.wav";
+    obj.volume=1;
+    obj.autoPlay=false;
+    obj.preLoad=true;
+
+    $("#six").click(function() {
+        obj.play();
+    });
+});
